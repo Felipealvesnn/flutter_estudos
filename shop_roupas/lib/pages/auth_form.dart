6f791a5b4fcd96130991ea7b7f0312490c86972d
Felipe Nunes Work <felipe.rudra@hotmail.com>
2023-08-08 16:53:39 -0300

    att

--- shop_roupas/lib/pages/auth_form.dart
+++ shop_roupas/lib/pages/auth_form.dart
@@ -15,15 +15,37 @@ class _auth_formState extends State<auth_form> {
     'email': '',
     'password': '',
   };
-  _submit() {
+bool _isLoading = false;
+  void _switchAuthMode() {
     if (_authMode == AuthMode.Login) {
-      // Login
+      setState(() => _authMode = AuthMode.Signup);
     } else {
-      // Registrar
+      setState(() => _authMode = AuthMode.Login);
     }
   }
-bool _isLogin() => _authMode == AuthMode.Login;
-bool _isSignup() => _authMode == AuthMode.Signup;
+final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  _submit() {
+    if (!_formKey.currentState!.validate()) {
+      return;
+    }
+    setState(() => _isLoading = true);
+    _formKey.currentState!.save();
+    if (_authMode == AuthMode.Login) {
+      // Provider.of<Auth>(context, listen: false).login(
+      //   _authData['email'] ?? '',
+      //   _authData['password'] ?? '',
+      // );
+    } else {
+      // Provider.of<Auth>(context, listen: false).signup(
+      //   _authData['email'] ?? '',
+      //   _authData['password'] ?? '',
+      // );
+    }
+    setState(() => _isLoading = false);
+  }
+
+  bool _isLogin() => _authMode == AuthMode.Login;
+  bool _isSignup() => _authMode == AuthMode.Signup;
   @override
   Widget build(BuildContext context) {
     final deviceSize = MediaQuery.of(context).size;
@@ -34,65 +56,62 @@ bool _isSignup() => _authMode == AuthMode.Signup;
           borderRadius: BorderRadius.circular(10),
         ),
         child: Container(
-          height: 300,
+          height: _isLogin() ? 310 : 400,
           width: deviceSize.width * 0.75,
           padding: const EdgeInsets.all(16),
           child: Form(
-            child: SingleChildScrollView(
-              child: Column(
-                children: [
+            key: _formKey,
+
+            child: Column(
+              children: [
+                TextFormField(
+                  validator: (value) => value!.isEmpty || !value.contains('@')
+                      ? 'Informe um e-mail válido!'
+                      : null,
+                  onSaved: (newValue) => _authData['email'] = newValue ?? '',
+                  decoration: const InputDecoration(labelText: 'E-mail'),
+                  keyboardType: TextInputType.emailAddress,
+                ),
+                TextFormField(
+                  onSaved: (newValue) => _authData['password'] = newValue ?? '',
+                  validator: (value) =>
+                      value!.length < 5 ? 'Senha muito curta!' : null,
+                  decoration: const InputDecoration(labelText: 'Senha'),
+                  obscureText: true,
+                  controller: TextEditingController(),
+                  keyboardType: TextInputType.emailAddress,
+                ),
+                if (_isSignup())
                   TextFormField(
-                    validator: (value) => value!.isEmpty || !value.contains('@')
-                        ? 'Informe um e-mail válido!'
+                    validator: (value) => value != _authData['password']
+                        ? 'Senhas são diferentes!'
                         : null,
-                    onSaved: (newValue) => _authData['email'] = newValue ?? '',
-                    decoration: const InputDecoration(labelText: 'E-mail'),
-                    keyboardType: TextInputType.emailAddress,
-                  ),
-                  TextFormField(
-                    onSaved: (newValue) =>
-                        _authData['password'] = newValue ?? '',
-                    validator: (value) =>
-                        value!.length < 5 ? 'Senha muito curta!' : null,
-                    decoration: const InputDecoration(labelText: 'Senha'),
+                    decoration:
+                        const InputDecoration(labelText: 'Confirmar Senha'),
                     obscureText: true,
                     controller: TextEditingController(),
                     keyboardType: TextInputType.emailAddress,
                   ),
-                  if (_isSignup())
-                    TextFormField(
-                      validator: 
-                           (value) => value != _authData['password']
-                              ? 'Senhas são diferentes!'
-                              : null,
-                      decoration:
-                          const InputDecoration(labelText: 'Confirmar Senha'),
-                      obscureText: true,
-                      controller: TextEditingController(),
-                      keyboardType: TextInputType.emailAddress,
-                    ),
-                  const SizedBox(height: 30),
-                  ElevatedButton(
-                      onPressed: () {},
-                      child: Text(
-                          _authMode == AuthMode.Login ? 'Entrar' : 'Registrar'),
-                      style: ElevatedButton.styleFrom(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30),
-                        ),
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 30.0, vertical: 8.0),
-                        textStyle: const TextStyle(
-                          color: Colors.white,
-                        ),
-                      )),
-                  const SizedBox(height: 20),
-                  TextButton(
-                    onPressed: _submit,
-                    child: const Text('Criar uma nova conta?'),
-                  ),
-                ],
-              ),
+                const SizedBox(height: 30),
+                ElevatedButton(
+                    onPressed: () => _submit(),
+                    child: Text(_isLogin() ? 'Entrar' : 'Registrar'),
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 30.0, vertical: 8.0),
+                      textStyle: const TextStyle(
+                        color: Colors.white,
+                      ),
+                    )),
+                const SizedBox(height: 20),
+                TextButton(
+                  onPressed: _switchAuthMode,
+                  child: const Text('Criar uma nova conta?'),
+                ),
+              ],
             ),
           ),
         ),
