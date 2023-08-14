cec0f7d44176445aa879354fab66ad3506b530db
Felipe Nunes Work <felipe.rudra@hotmail.com>
2023-08-14 09:52:01 -0300

    add an imacoes

--- shop_roupas/lib/pages/auth_form.dart
+++ shop_roupas/lib/pages/auth_form.dart
@@ -23,7 +23,8 @@ class _auth_formState extends State<auth_form>
   };
   bool _isLoading = false;
   AnimationController? _controller;
-  Animation<Size>? _heightAnimation;
+  Animation<double>? _opacityAnimation;
+  Animation<Offset>? _slideAnimation;
 
   @override
   void initState() {
@@ -34,162 +35,186 @@ class _auth_formState extends State<auth_form>
       duration: const Duration(milliseconds: 500),
     );
 
-    _heightAnimation = Tween<Size>(
-      begin: Size(double.infinity, 310),
-      end: Size(double.infinity, 410),
+    _opacityAnimation = Tween(
+      begin: 0.0,
+      end: 1.0,
     ).animate(
       CurvedAnimation(
         parent: _controller!,
         curve: Curves.linear,
       ),
     );
-
-    _heightAnimation!.addListener(() => setState(() {}));
-  }
-
-  @override
-  void dispose() {
-    // TODO: implement dispose
-    super.dispose();
-    _controller!.dispose();
-  }
-
-  void _switchAuthMode() {
-    if (_authMode == AuthMode.Login) {
-      setState(() {
-        _authMode = AuthMode.Signup;
-        _controller!.forward();
-      });
-    } else {
-      setState(() {
-        _authMode = AuthMode.Login;
-        _controller!.reverse();
-      });
-    }
-  }
-
-  void _showErroDialog(String msg) {
-    showDialog(
-      context: context,
-      builder: (ctx) => AlertDialog(
-        title: const Text('Ocorreu um erro!'),
-        content: Text(msg),
-        actions: [
-          TextButton(
-            onPressed: () => Navigator.of(context).pop(),
-            child: const Text('Fechar'),
-          ),
-        ],
+    _slideAnimation = Tween<Offset>(
+      begin: const Offset(0, -1.5),
+      end: const Offset(0, 0),
+    ).animate(
+      CurvedAnimation(
+        parent: _controller!,
+        curve: Curves.linear,
       ),
     );
   }
-
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-  Future<void> _submit() async {
-    if (!_formKey.currentState!.validate()) {
-      return;
+    @override
+    void dispose() {
+      // TODO: implement dispose
+      super.dispose();
+      _controller!.dispose();
     }
-    final provider = Provider.of<Auth>(context, listen: false);
-    setState(() => _isLoading = true);
-    _formKey.currentState!.save();
-    try {
+
+    void _switchAuthMode() {
       if (_authMode == AuthMode.Login) {
-        await provider.login(
-          _authData['email'] ?? '',
-          _authData['password'] ?? '',
-        );
+        setState(() {
+          _authMode = AuthMode.Signup;
+          _controller!.forward();
+        });
       } else {
-        await provider.signup(
-          _authData['email'] ?? '',
-          _authData['password'] ?? '',
-        );
+        setState(() {
+          _authMode = AuthMode.Login;
+          _controller!.reverse();
+        });
       }
-    } catch (error) {
-      setState(() => _isLoading = false);
-      _showErroDialog(error.toString());
     }
-    setState(() => _isLoading = false);
+
+    void _showErroDialog(String msg) {
+      showDialog(
+        context: context,
+        builder: (ctx) => AlertDialog(
+          title: const Text('Ocorreu um erro!'),
+          content: Text(msg),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: const Text('Fechar'),
+            ),
+          ],
+        ),
+      );
+    }
+
+    final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+    Future<void> _submit() async {
+      if (!_formKey.currentState!.validate()) {
+        return;
+      }
+      final provider = Provider.of<Auth>(context, listen: false);
+      setState(() => _isLoading = true);
+      _formKey.currentState!.save();
+      try {
+        if (_authMode == AuthMode.Login) {
+          await provider.login(
+            _authData['email'] ?? '',
+            _authData['password'] ?? '',
+          );
+        } else {
+          await provider.signup(
+            _authData['email'] ?? '',
+            _authData['password'] ?? '',
+          );
+        }
+      } catch (error) {
+        setState(() => _isLoading = false);
+        _showErroDialog(error.toString());
+      }
+      setState(() => _isLoading = false);
+    }
+
+    bool _isLogin() => _authMode == AuthMode.Login;
+    bool _isSignup() => _authMode == AuthMode.Signup;
+    @override
+    Widget build(BuildContext context) {
+      final deviceSize = MediaQuery.of(context).size;
+      return Center(
+        child: Card(
+            elevation: 8,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: AnimatedContainer(
+              duration: const Duration(milliseconds: 500),
+              curve: Curves.linear,
+              height: _isLogin() ? 300 : 410,
+              // height:
+              // _heightAnimation!.value.height ?? (_isLogin() ? 300 : 410),
+              width: deviceSize.width * 0.75,
+              padding: const EdgeInsets.all(16),
+              child: SingleChildScrollView(
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        validator: (value) =>
+                            value!.isEmpty || !value.contains('@')
+                                ? 'Informe um e-mail válido!'
+                                : null,
+                        onSaved: (newValue) =>
+                            _authData['email'] = newValue ?? '',
+                        decoration: const InputDecoration(labelText: 'E-mail'),
+                        keyboardType: TextInputType.emailAddress,
+                      ),
+                      TextFormField(
+                        onSaved: (newValue) =>
+                            _authData['password'] = newValue ?? '',
+                        validator: (value) =>
+                            value!.length < 5 ? 'Senha muito curta!' : null,
+                        decoration: const InputDecoration(labelText: 'Senha'),
+                        obscureText: true,
+                        controller: TextEditingController(),
+                        keyboardType: TextInputType.emailAddress,
+                      ),
+                      AnimatedContainer(
+                        constraints: BoxConstraints(
+                          minHeight: _isLogin() ? 0 : 60,
+                          maxHeight: _isLogin() ? 0 : 120,
+                        ),
+                        curve: Curves.linear,
+                        duration: const Duration(milliseconds: 800),
+                        child: FadeTransition(
+                          opacity: _opacityAnimation!,
+                          child: SlideTransition(
+                            position: _slideAnimation!,
+                            child: TextFormField(
+                              validator: (value) {
+                                value != _authData['password']
+                                    ? 'Senhas são diferentes!'
+                                    : null;
+                              },
+                              decoration: const InputDecoration(
+                                  labelText: 'Confirmar Senha'),
+                              obscureText: true,
+                              controller: TextEditingController(),
+                              keyboardType: TextInputType.emailAddress,
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 30),
+                      _isLoading
+                          ? const CircularProgressIndicator()
+                          : ElevatedButton(
+                              onPressed: () async => await _submit(),
+                              child: Text(_isLogin() ? 'Entrar' : 'Registrar'),
+                              style: ElevatedButton.styleFrom(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(30),
+                                ),
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 30.0, vertical: 8.0),
+                                textStyle: const TextStyle(
+                                  color: Colors.white,
+                                ),
+                              )),
+                      const SizedBox(height: 20),
+                      TextButton(
+                        onPressed: _switchAuthMode,
+                        child: const Text('Criar uma nova conta?'),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            )),
+      );
+    }
   }
 
-  bool _isLogin() => _authMode == AuthMode.Login;
-  bool _isSignup() => _authMode == AuthMode.Signup;
-  @override
-  Widget build(BuildContext context) {
-    final deviceSize = MediaQuery.of(context).size;
-    return Center(
-      child: Card(
-        elevation: 8,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10),
-        ),
-        child: Container(
-          //height: _isLogin() ? 300 : 410,
-          height: _heightAnimation!.value.height ?? (_isLogin() ? 300 : 410),
-          width: deviceSize.width * 0.75,
-          padding: const EdgeInsets.all(16),
-          child: SingleChildScrollView(
-            child: Form(
-              key: _formKey,
-              child: Column(
-                children: [
-                  TextFormField(
-                    validator: (value) => value!.isEmpty || !value.contains('@')
-                        ? 'Informe um e-mail válido!'
-                        : null,
-                    onSaved: (newValue) => _authData['email'] = newValue ?? '',
-                    decoration: const InputDecoration(labelText: 'E-mail'),
-                    keyboardType: TextInputType.emailAddress,
-                  ),
-                  TextFormField(
-                    onSaved: (newValue) =>
-                        _authData['password'] = newValue ?? '',
-                    validator: (value) =>
-                        value!.length < 5 ? 'Senha muito curta!' : null,
-                    decoration: const InputDecoration(labelText: 'Senha'),
-                    obscureText: true,
-                    controller: TextEditingController(),
-                    keyboardType: TextInputType.emailAddress,
-                  ),
-                  if (_isSignup())
-                    TextFormField(
-                      validator: (value) {
-                        value != _authData['password']
-                            ? 'Senhas são diferentes!'
-                            : null;
-                      },
-                      decoration:
-                          const InputDecoration(labelText: 'Confirmar Senha'),
-                      obscureText: true,
-                      controller: TextEditingController(),
-                      keyboardType: TextInputType.emailAddress,
-                    ),
-                  const SizedBox(height: 30),
-                  _isLoading
-                      ? const CircularProgressIndicator()
-                      : ElevatedButton(
-                          onPressed: () async => await _submit(),
-                          child: Text(_isLogin() ? 'Entrar' : 'Registrar'),
-                          style: ElevatedButton.styleFrom(
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(30),
-                            ),
-                            padding: const EdgeInsets.symmetric(
-                                horizontal: 30.0, vertical: 8.0),
-                            textStyle: const TextStyle(
-                              color: Colors.white,
-                            ),
-                          )),
-                  const SizedBox(height: 20),
-                  TextButton(
-                    onPressed: _switchAuthMode,
-                    child: const Text('Criar uma nova conta?'),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
